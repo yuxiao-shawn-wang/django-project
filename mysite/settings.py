1d73ae4e5e778aef1e1fef8bee45f627ca99f152
Yuxiao Shawn Wang <shawnwang@dyn-160-39-148-80.dyn.columbia.edu>
2019-12-03 21:55:46 -0500

    Created new app 'sightings', added ALLOWED_HOSTS in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm+)o!z5ki9!tl0ji_fuzut_zcngrka4uerfyx78d@kpax$q#zi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sightings',
 ]
 
 MIDDLEWARE = [
