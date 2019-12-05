ae3fe30fe9646cc8c466cb72de84f277f3cfc010
Shaelyn <xiaoyanyao84@gmail.com>
2019-12-05 02:33:56 +0000

    include the url for update and delete

---
+++ sightings/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'sightings'
+urlpatterns = [
+        # add an url path that can view all the sightings
+    path('', views.all_sightings, name='list'),
+    # add an url path than can view and update  the ditail of a specific sighting
+    path('<Unique_Squirrel_ID>', views.squirrel_details, name='detail'),
+    path('<Unique_Squirrel_ID>/update', views.squirrel_details, name = 'update'),
+    # add the url path to delete the sighting
+    path('<Unique_Squirrel_ID>/delete', views.delete, name='delete'),
+    ]
+
