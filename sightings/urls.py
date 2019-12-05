a9279e2b0d75a6bce43d0e0b5a90e50cc3858abb
Yuxiao Shawn Wang <shawnwang@Yuxiaos-MacBook-Air.local>
2019-12-05 15:35:49 -0500

    Add stats views under sightigns app.

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 app_name = 'sightings'
 urlpatterns = [
-        # add an url path that can view all the sightings
+    # add an url path that can view all the sightings
     path('', views.all_sightings, name='list'),
+    # add an url path that can view the stats about Squirrel Data
+    path('stats', views.stats, name='stats'),
     # add an url path than can view and update  the ditail of a specific sighting
     path('<Unique_Squirrel_ID>', views.squirrel_details, name='detail'),
     path('<Unique_Squirrel_ID>/update', views.squirrel_details, name = 'update'),
--- sightings/views.py
+++ sightings/views.py
@@ -33,3 +33,17 @@ def delete(request, Unique_Squirrel_ID):
     squirrel = get_object_or_404(Squirrel, Unique_Squirrel_ID=Unique_Squirrel_ID)
     squirrel.delete()
     return HttpResponse('Successfully Delete it!')
+
+def stats(request):
+    squirrels = Squirrel.objects.all()
+    field_name = [x.name for x in Squirrel._meta.fields 
+        if x.name not in {'Latitude','Longitude','Unique_Squirrel_ID','Date','Specific_Location','Other_Activities'}]
+    field_values = {y:{} for y in field_name}
+    for field in field_values.keys():
+        for val_pari in squirrels.order_by().values(field).distinct():
+            val = val_pari[field]
+            criteria = {field:val}
+            field_values[field][val] = squirrels.filter(**criteria).count()
+
+    context = {'field_values':field_values}
+    return render(request, 'sightings/stats.html', context)
